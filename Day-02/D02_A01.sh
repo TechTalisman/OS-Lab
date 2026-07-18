508325fcc5c054c6032ade48260eae86f6ef51a1
Manas Kumar <manaskr501@gmail.com>
2026-07-18 11:11:35 +0530

    Add script to display user and system details

---
+++ Day-02/D02_A01.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Display the current username
+echo "Current username: $(whoami)"
+
+# Display the current date and time
+echo "Current date and time: $(date)"
+
+# Display the name of the shell
+echo "Name of the shell: $SHELL"
+
+# Display the current working directory
+echo "Current working directory: $(pwd)"
