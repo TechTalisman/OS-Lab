3ddc10d8e431143188b15b80f6c245e3ad7c707e
Manas Kumar <manaskr501@gmail.com>
2026-07-19 10:29:55 +0530

    Add user input script for name, age, and language

---
+++ Day-02/D02_A02.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Enter your name "
+read name
+echo "Enter your age "
+read age
+echo "Enter your favorite programming language "
+read prog
+
+echo "Name is $name"
+echo "Age is $age"
+echo "Favorite programming language is $prog"
