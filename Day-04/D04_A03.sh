070dc97b3e1c59e0d68a3b8b6cf2d0ab150f8a2e
Manas Kumar <manaskr501@gmail.com>
2026-08-02 10:43:41 +0530

    Add number reversal script in D04_A03.sh
    This script prompts the user for a number and reverses its digits.

---
+++ Day-04/D04_A03.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter a number:"
+read number
+
+reverse=0
+while [ $number -gt 0 ]; do
+    digit=$((number % 10))
+    reverse=$((reverse * 10 + digit))
+    number=$((number / 10))
+done
+
+echo "Reversed Number: $reverse"
